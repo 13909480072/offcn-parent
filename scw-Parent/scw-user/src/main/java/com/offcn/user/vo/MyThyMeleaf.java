d902990e377eb8938ed1c016135416471157ce9c
wsw <13909480072@163.com>
2020-10-29 17:43:03 +0800

    idea第二次提交

---
+++ scw-Parent/scw-user/src/main/java/com/offcn/user/vo/MyThyMeleaf.java
@@ -0,0 +1,10 @@
+package com.offcn.user.vo;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyThyMeleaf {
+    public String returnDemo(){
+        return "mydemo";
+    }
+}
